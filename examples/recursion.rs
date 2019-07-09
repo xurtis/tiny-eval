dba6dbaf68109b1061d72d9fb55ecbff415b6043
Curtis Millar <curtis@curtism.me>
2019-07-09 11:47:03 +1000

    Use take for recursion example

--- examples/recursion.rs
+++ examples/recursion.rs
@@ -3,39 +3,18 @@ use tiny_eval::*;
 type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
 fn main() -> Result<()> {
-    apply_expr!(index(i, xs));
+    apply_expr!(take(n, xs));
 
-    // let
-    //   fib := let
-    //     fib := a -> b -> let
-    //       next := (+ a b)
-    //       in
-    //         (pair next (fib b next))
-    //     in
-    //       (pair 1 (pair 1 (fib 1 1)))
-    //   in let
-    //     index := i -> xs ->
-    //       if (= i 0)
-    //         (first xs)
-    //         (index (- i 1) (second xs))
-    //   in
-    //     (pair
-    //       (index 0 fib)
-    //       (pair
-    //         (index 1 fib)
-    //         (pair
-    //           (index 2 fib)
-    //           (pair
-    //             (index 3 fib)
-    //             (pair
-    //               (index 4 fib)
-    //               (index 5 fib)
-    //             )
-    //           )
-    //         )
-    //       )
-    //     )
-    // = (1 * 1 * 2 * 3 * 5 * 8)
+    // let fib :=
+    //   let fib := a -> b ->
+    // 	let next := (+ a b)
+    // 	  in (pair next (fib b next))
+    //     in (pair 1 (pair 1 (fib 1 1)))
+    //   in let take := count -> list ->
+    //     if (= count 0)
+    // 	  ()
+    // 	  (pair (first list) (take (- count 1) (second list)))
+    //     in (take 35 fib)
 
     let expr = bind(
         "fib", bind(
@@ -46,32 +25,17 @@ fn main() -> Result<()> {
             pair(value(1), pair(value(1), apply(apply("fib", value(1)), value(1)))),
         ),
         bind(
-            "index", lambda("i", lambda("xs",
+            "take", lambda("count", lambda("list",
                 condition(
-                    equal("i", value(0)),
-                    first("xs"),
-                    index(subtract("i", value(1)), second("xs")),
+                    equal("count", value(0)),
+                    value(()),
+                    pair(
+                        first("list"),
+                        take(subtract("count", value(1)), second("list")),
+                    ),
                 )
             )),
-            pair(
-                index(value(0), "fib"),
-                pair(
-                    index(value(1), "fib"),
-                    pair(
-                        index(value(2), "fib"),
-                        pair(
-                            index(value(3), "fib"),
-                            pair(
-                                index(value(4), "fib"),
-                                pair(
-                                    index(value(5), "fib"),
-                                    value(()),
-                                ),
-                            ),
-                        ),
-                    ),
-                ),
-            )
+            take(value(35), "fib"),
         )
     );
 
